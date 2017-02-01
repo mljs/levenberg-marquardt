f2a2e878cf2567a50e7852cf2bcf0933d6149358
maasencioh <maasencioh@gmail.com>
2017-01-31 21:47:33 -0500

    feat: check that the data has the correct format

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 'use strict';
 
-const defaultOptions = {};
+const defaultOptions = {
+    dampingIncrease: 10e-2,
+    dampingDecrease: 10e-2,
+    maxIterations: 100,
+    errorTolerance: 10e-3
+};
 
-function levenbergMarquardt(options) {
+function levenbergMarquardt(data, initialValues, damping, parameterizedFunction, options) {
     options = Object.assign({}, defaultOptions, options);
+
+    // check that the data has the correct format
+    if ((!Array.isArray(data)) || (data.length !== 2)) {
+        throw new TypeError('The data parameter should be an array of size 2');
+    } else if ((!Array.isArray(data[0])) || (data[0].length < 2)
+        || (!Array.isArray(data[1])) || (data[1].length < 2)) {
+        throw new TypeError('The data parameter elements should be an array with more than 2 points');
+    }
+
+    const dataLen = data[0].length;
+    if (dataLen !== data[1].length) {
+        throw new RangeError('The data parameter elements should have the same size');
+    }
 }
 
 module.exports = levenbergMarquardt;
