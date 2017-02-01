b76c8478a3f60be91cc76a920f1cfa75a795c98c
maasencioh <maasencioh@gmail.com>
2017-01-31 20:56:14 -0500

    test: add basic test to be implemented

--- test/test.js
+++ test/test.js
@@ -4,7 +4,20 @@ const levenbergMarquardt = require('..');
 
 describe('levenberg-marquardt test', function () {
 
-    it('Something to test', function () {
-        (true).should.be.equal(true);
+    it('Base example', function () {
+        function sinFunction(a, b) {
+            return (t) => (a * Math.sin(b * t));
+        }
+
+        const len = 20;
+        let data = new Array(len);
+        let sampleFunction = sinFunction(2, 2);
+        for (let i = 0; i < len; i++) {
+            data[i] = [i, sampleFunction(i)];
+        }
+
+        let params = levenbergMarquardt(data, [1, 1], 0.1, sinFunction);
+        params[0].should.be.approximately(2, 10e-3);
+        params[1].should.be.approximately(2, 10e-3);
     });
 });
