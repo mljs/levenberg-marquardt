e0a6a5085e2987c8b645ee79c4e9d960f8f96fa6
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2017-12-08 08:11:18 -0500

    test: fix errored example
    closes #7

--- runkit.js
+++ runkit.js
@@ -1,6 +1,4 @@
-'use strict';
-
-var levenbergMarquardt = require('ml-levenberg-marquardt');
+const levenbergMarquardt = require('ml-levenberg-marquardt');
 
 function sinFunction([a, b]) {
     return (t) => (a * Math.sin(b * t));
@@ -11,7 +9,7 @@ var data = {
     x: new Array(len),
     y: new Array(len)
 };
-var sampleFunction = sinFunction(2, 2);
+var sampleFunction = sinFunction([2, 2]);
 for (var i = 0; i < len; i++) {
     data.x[i] = i;
     data.y[i] = sampleFunction(i);
