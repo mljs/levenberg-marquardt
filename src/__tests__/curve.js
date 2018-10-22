2fd7bb8bc64397b2f2d79712682b50c1b2d9d07f
Jacob Quant <jacobq@gmail.com>
2018-10-22 17:33:54 -0500

    refactor: move curve fit tests from test.js to curve.js
    It wasn't clear to me why some curve fitting tests were written in curve.js while others were in
    test.js. This commit moves the sin, sigmoid, and sum of lorentizians test cases to the curve.js file
    and adapts them to reuse the existing structure.

--- src/__tests__/curve.js
+++ src/__tests__/curve.js
@@ -8,7 +8,6 @@ describe('Contrived problems (clean data)', () => {
   // In these cases we test the algorithm's ability to find an , we use some pre-selected values and generate the data set and see if the algorithm can get close the the exact solution
   const contrivedProblems = [
     {
-      skip: false,
       name: 'bennet5([2, 3, 5])',
       getFunctionFromParameters: ([b1, b2, b3]) => ((t) => b1 * Math.pow(t + b2, -1 / b3)),
       n: 154,
@@ -23,14 +22,65 @@ describe('Contrived problems (clean data)', () => {
         minBound: [1, 1, 1],
         initialValues: [3.5, 3.8, 4]
       }
+    }, {
+      name: '2*sin(2*t)',
+      getFunctionFromParameters: ([a, f]) => ((t) => a * Math.sin(f * t)),
+      n: 20,
+      xStart: 0,
+      xEnd: 19,
+      problemParameters: [2, 2],
+      options: {
+        damping: 0.1,
+        initialValues: [3, 3]
+      }
+    }, {
+      name: 'Sigmoid',
+      getFunctionFromParameters: ([a, b, c]) => (t) => a / (b + Math.exp(-t * c)),
+      n: 20,
+      xStart: 0,
+      xEnd: 19,
+      problemParameters: [2, 2, 2],
+      options: {
+        damping: 0.1,
+        initialValues: [3, 3, 3],
+        maxIterations: 200
+      },
+      decimalsForParameterValues: 1
+    }, {
+      name: 'Sum of lorentzians',
+      getFunctionFromParameters: function sumOfLorentzians(p) {
+        return (t) => {
+          var nL = p.length;
+          var factor, p2;
+          var result = 0;
+          for (var i = 0; i < nL; i += 3) {
+            p2 = Math.pow(p[i + 2] / 2, 2);
+            factor = p[i + 1] * p2;
+            result += factor / (Math.pow(t - p[i], 2) + p2);
+          }
+          return result;
+        };
+      },
+      n: 100,
+      xStart: 0,
+      xEnd: 99,
+      problemParameters: [1, 0.1, 0.3, 4, 0.15, 0.3],
+      options: {
+        damping: 0.01,
+        initialValues: [1.1, 0.15, 0.29, 4.05, 0.17, 0.28],
+        maxIterations: 500,
+        errorTolerance: 10e-5
+      },
+      decimalsForParameterValues: 1
     }
   ];
 
   contrivedProblems.forEach((problem) => {
     const testInvocation = (problem.skip) ? it.skip.bind(it) : it;
     testInvocation(`Should fit ${problem.name}`, () => {
-      const { getFunctionFromParameters, n, xStart, xEnd, problemParameters, options, decimals } = Object.assign({
-        decimals: 3
+      const { getFunctionFromParameters, n, xStart, xEnd, problemParameters, options, decimalsForParameterError, decimalsForParameterValues } = Object.assign({
+        decimalsForParameterError: 2,
+        decimalsForParameterValues: 3
       }, problem);
       const xs = new Array(n).fill(0).map((zero, i) => xStart + (i * (xEnd - xStart)) / (n - 1));
       const data = {
@@ -39,7 +89,8 @@ describe('Contrived problems (clean data)', () => {
       };
 
       const actual = levenbergMarquardt(data, getFunctionFromParameters, options);
-      expect(actual.parameterValues).toBeDeepCloseTo(problemParameters, decimals);
+      expect(actual.parameterValues).toBeDeepCloseTo(problemParameters, decimalsForParameterValues);
+      expect(actual.parameterError).toBeDeepCloseTo(0, decimalsForParameterError);
     });
   });
 });
--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -5,31 +5,6 @@ import levenbergMarquardt from '..';
 expect.extend({ toBeDeepCloseTo });
 
 describe('levenberg-marquardt test', () => {
-  it('Base example', () => {
-    const len = 20;
-    let data = {
-      x: new Array(len),
-      y: new Array(len)
-    };
-    let sampleFunction = sinFunction([2, 2]);
-    for (let i = 0; i < len; i++) {
-      data.x[i] = i;
-      data.y[i] = sampleFunction(i);
-    }
-    const options = {
-      damping: 0.1,
-      initialValues: [3, 3]
-    };
-
-    const { parameterValues, parameterError } = levenbergMarquardt(
-      data,
-      sinFunction,
-      options
-    );
-    expect(parameterValues).toBeDeepCloseTo([2, 2], 3);
-    expect(parameterError).toBeCloseTo(0, 2);
-  });
-
   it('Exceptions', () => {
     const options = {
       damping: 0.1,
@@ -57,64 +32,10 @@ describe('levenberg-marquardt test', () => {
       })
     ).toThrow('initialValues must be an array');
   });
-
-  it('Sigmoid example', () => {
-    const len = 20;
-    let data = {
-      x: new Array(len),
-      y: new Array(len)
-    };
-    let sampleFunction = sigmoidFunction([2, 2, 2]);
-    for (let i = 0; i < len; i++) {
-      data.x[i] = i;
-      data.y[i] = sampleFunction(i);
-    }
-    const options = {
-      damping: 0.1,
-      initialValues: [3, 3, 3],
-      maxIterations: 200
-    };
-
-    let { parameterValues, parameterError } = levenbergMarquardt(
-      data,
-      sigmoidFunction,
-      options
-    );
-    expect(parameterValues).toBeDeepCloseTo([2, 2, 2], 1);
-    expect(parameterError).toBeCloseTo(0, 1);
-  });
-
-  it('Sum of lorentzians example', () => {
-    const len = 100;
-    const pTrue = [1, 0.1, 0.3, 4, 0.15, 0.3];
-    let data = {
-      x: new Array(len),
-      y: new Array(len)
-    };
-
-    let sampleFunction = sumOfLorentzians(pTrue);
-    for (let i = 0; i < len; i++) {
-      data.x[i] = i;
-      data.y[i] = sampleFunction(i);
-    }
-
-    let { parameterValues } = levenbergMarquardt(data, sumOfLorentzians, {
-      damping: 0.01,
-      initialValues: [1.1, 0.15, 0.29, 4.05, 0.17, 0.28],
-      maxIterations: 500,
-      errorTolerance: 10e-5
-    });
-
-    expect(parameterValues).toBeDeepCloseTo(pTrue, 1);
-  });
 });
 
 function sinFunction([a, b]) {
   return (t) => a * Math.sin(b * t);
-}
-
-function sigmoidFunction([a, b, c]) {
-  return (t) => a / (b + Math.exp(-t * c));
 }
 
 function sumOfLorentzians(p) {
