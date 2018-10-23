9c99709b23fa5d027fc8da0e3d228e6934d56217
Jacob Quant <jacobq@gmail.com>
2018-10-23 09:01:10 -0500

    refactor(errorCalculation): use linear function instead of sinusoid for test
    Although the Levenberg-Marquardt algorithm is generally used to fit non-linear functions, there is
    no need to use a non-linear function in the unit test suite for the residual/error calculation code.
    That computation is currently a simple sum of (absolute) differences, which can be tested with
    simpler functions (even a constant one). This commit demonstrates that there is no reduction in code
    coverage, yet the test case is easier to reason about.

--- src/__tests__/errorCalculation.js
+++ src/__tests__/errorCalculation.js
@@ -1,7 +1,31 @@
 import errorCalculation from '../errorCalculation';
 
 describe('parameterError', () => {
-  describe('Sinusoidal functions', () => {
+  describe('Linear functions', () => {
+    const linearFunction = ([slope, intercept]) => ((x) => slope * x + intercept);
+
+    const sampleParameters = [1, 1];
+    const n = 10;
+    const xs = new Array(n).fill(0).map((zero, i) => i);
+    const data = {
+      x: xs,
+      y: xs.map(linearFunction(sampleParameters))
+    };
+
+    it('parameterError should be zero for an exact fit', () => {
+      expect(errorCalculation(data, sampleParameters, linearFunction)).toBeCloseTo(0, 3);
+    });
+
+    it('parameterError should match the sum of absolute difference between the model and the data', () => {
+      const parameters = Array.from(sampleParameters);
+      // Offset line so that it's still parallel but differs by 1 at each point
+      // Then each point will result in a residual increase of 1
+      parameters[1] += 1;
+      expect(errorCalculation(data, parameters, linearFunction)).toBeCloseTo(n, 3);
+    });
+  });
+
+  describe.skip('Sinusoidal functions', () => {
     function sinFunction([a, b]) {
       return (t) => a * Math.sin(b * t);
     }
