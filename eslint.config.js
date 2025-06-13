4accbfaeb2c5c97696e0211b2454148426653b98
Michaël Zasso <targos@protonmail.com>
2025-06-13 08:47:45 +0200

    chore: remove useless benchmark

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,4 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
 import ts from 'eslint-config-cheminfo-typescript/base';
 
-export default defineConfig(
-  globalIgnores(['coverage', 'lib', 'benchmark']),
-  ts,
-);
+export default defineConfig(globalIgnores(['coverage', 'lib']), ts);
